8d139950abbfbc4c03bdd8d1f7411fef4f6af834
Nima karimi <nima.karimi@live.com>
2018-01-27 11:22:02 +0330

    'ClassBaseSlugUrsl'

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,24 +1,28 @@
+from django.db.models import Q
+from django.http import HttpResponse , Http404
 from django.shortcuts import render
-from django.views.generic import TemplateView , ListView
 from django.views import View
+from django.views.generic import TemplateView , ListView
+
 from .models import RestaurantLocation
 
-# def restaurant_listview(request):
-#     template_name = 'restaurants/restaurants_list.html'
-#     queryset = RestaurantLocation.objects.all()
-#     context = {
-#         "object_list" : queryset
-#     }
-#     return render(request, template_name, context)
+def restaurant_listview(request):
+    template_name = 'restaurants/restaurants_list.html'
+    queryset = RestaurantLocation.objects.all()
+    context = {
+        "object_list" : queryset
+    }
+    return render(request, template_name, context)
 
 class RestaurantListView(ListView):
-    queryset = RestaurantLocation.objects.all()
-    template_name = 'restaurants/restaurants_list.html'
-
-
-class SearchRestaurantListView(ListView):
-    template_name = 'restaurants/restaurants_list.html'
     def get_queryset(self):
-        print(self.kwargs)
-        queryset = RestaurantLocation.objects.filter(category__iexact='pizza')
+        #print(self.kwargs)
+        slug = self.kwargs.get("slug")
+        if slug:
+            queryset = RestaurantLocation.objects.filter(
+                Q(category__iexact=slug) |
+                Q(category__icontains=slug)
+            )
+        else:
+            queryset = RestaurantLocation.objects.all()
         return queryset
